52e76c62fe05e7be736a1924565d43580165838b
Jorge Luiz Costa da Fonseca Junior <48517415+jorgefonsecajr@users.noreply.github.com>
2020-10-07 11:00:26 -0300

    Add files via upload

---
+++ crossroad/images.js
@@ -0,0 +1,26 @@
+//codigo imagens e sons
+
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
+
+
+function preload(){
+    imagemDaEstrada = loadImage("images/estrada.png");
+    imagemDoAtor = loadImage("images/ator-1.png");
+    imagemCarro = loadImage("images/carro-1.png");
+    imagemCarro2 = loadImage("images/carro-2.png");
+    imagemCarro3 = loadImage("images/carro-3.png");
+    imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3]
+
+    somDaTrilha = loadSound("sounds/trilha.mp3");
+    somDaColisao = loadSound("sounds/colidiu.mp3");
+    somDoPonto = loadSound("sounds/pontos.wav");
+}
+
